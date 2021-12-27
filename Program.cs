93b950383e7c427963042992747c8ae6178c8d70
Penguin-God <gkswh4860@naver.com>
2021-12-27 16:21:14 +0900

    강의 정리

--- Program.cs
+++ Program.cs
@@ -5,19 +5,53 @@ using System.Reflection;
 
 namespace Data_Basic
 {
-	class Program
+    class Program
     {
+
+        delegate void Del_Test(int a);
+
+        static void OnCilcked(Action action)
+        {
+            Console.WriteLine("Hello Delegate");
+            action();
+        }
+
+        static void TestDel()
+        {
+            Console.WriteLine("1");
+        }
+
+        static void TestDel2()
+        {
+            Console.WriteLine("2");
+        }
+
+        class MainPlayer
+        {
+            int hp  = 100;
+            public int Hp
+            {
+                get { return hp; }
+                set { hp = value; }
+            }
+
+            public long GameID { get; }
+            public bool IsDead { get { return hp <= 0; } }
+
+            // 자동 구현 및 초기화 위에 Hp와 완전히 똑같음 기본은 public
+            public int HP { get; set; } = 100;
+
+            // 하나 없애는 것도 가능
+            public int NoChangeHP { get; } = 100;
+        }
+
         static void Main(string[] args)
         {
-            List<int> Int_List = new List<int>() { 1,3,3,2 };
+            Action act_test = null;
+            act_test += TestDel;
+            act_test += TestDel2;
 
-            List<int> Int_List_2 = Int_List;
-
-            List<int> DeepCopy_List = new List<int>(Int_List);
-
-            Int_List[0] = 200;
-            Console.WriteLine(Int_List_2[0]);
-            Console.WriteLine(DeepCopy_List[0]);
+            OnCilcked(act_test);
         }
     }
 }
